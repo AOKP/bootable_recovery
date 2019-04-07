LineageOS/android_bootable_recovery

Fix making adb use a custom prop for adb root
Change-Id: I72713e78addad67854144bb3fbba08521dee68a6

recovery.cpp
@@ -1524,7 +1524,7 @@ setup_adbd() {
     ensure_path_unmounted("/data");
 
     // Trigger (re)start of adb daemon
-    property_set("service.adb.root", "1");
+    property_set("cm.service.adb.root", "1");
 }
 
 static RecoveryUI* gCurrentUI = NULL;

AOKP/bootable_recovery

recovery.cpp
@@ -1575,7 +1575,7 @@ static void setup_adbd() {
     }
 
     // Trigger (re)start of adb daemon
-    property_set("service.adb.root", "1");
+    property_set("lineage.service.adb.root", "1");
 }
 
 int main(int argc, char **argv) {